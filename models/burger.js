130b94919e52ade69f7cad2d7d91c27ffa49461f
a-h <alexandra.k.hinton1@gmail.com>
2021-01-16 15:52:59 -0500

    comment out incomplete code

--- models/burger.js
+++ models/burger.js
@@ -9,15 +9,15 @@ var burger = {
  
     // The variables cols and vals are arrays.
     insertOne: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
-        cb(res);
-      });
+      // orm.create("burgers", cols, vals, function(res) {
+      //   cb(res);
+      // });
     },
     
     updateOne: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
-        cb(res);
-      });
+      // orm.update("burgers", objColVals, condition, function(res) {
+      //   cb(res);
+      // });
     },
 
     deleteOne: async function (columnName, columnValue, cb) {
